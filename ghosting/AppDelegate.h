c3eaa4a869340eb18c7a41e9f5f1b8e24ec73ff7
George Nachman <gnachman@gmail.com>
2018-10-03 21:48:54 -0700

    Initial Commit

---
+++ ghosting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ghosting
+//
+//  Created by George Nachman on 10/3/18.
+//  Copyright © 2018 George Nachman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ghosting/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ghosting
+//
+//  Created by George Nachman on 10/3/18.
+//  Copyright © 2018 George Nachman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
